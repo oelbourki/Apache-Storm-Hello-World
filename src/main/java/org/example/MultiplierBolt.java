edf143cc24da08ffe0e404f8663d3e712cbd796d
oelbourki <elbourki.1999@gmail.com>
2023-01-24 20:58:21 +0100

    hello world

---
+++ src/main/java/org/example/MultiplierBolt.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.apache.storm.topology.BasicOutputCollector;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+
+public class MultiplierBolt extends BaseBasicBolt {
+    @Override
+    public void execute(Tuple tuple, BasicOutputCollector basicOutputCollector) {
+        Integer number = tuple.getInteger(0);
+        number *= 2;
+        basicOutputCollector.emit(new Values(number));
+        System.out.println("number:" + number.toString());
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+    outputFieldsDeclarer.declare(new Fields("field"));
+    }
+}
